3a8342dac0f636ee4d3ca5b6d9f5b4a2ffcf1256
gurjaspreet <107759543+gurjaspreet@users.noreply.github.com>
2023-06-08 10:27:55 +0530

    Day 1 completed :)

--- Day 1/Exercise1.py
+++ Day 1/Exercise1.py
@@ -1,20 +1,4 @@
-'''  
-Exercise 1
-
-Using NumPy create a 5x5 array filled with zeros (set data type to int16) and assign it to a variable named arr. In response, print this array to the console.
-
-Expected output:
-
-    [[0 0 0 0 0]
-     [0 0 0 0 0]
-     [0 0 0 0 0]
-     [0 0 0 0 0]
-     [0 0 0 0 0]]
-
-Tip: Use the np.zeros() function.   
-
-'''
-
 import numpy as np
+
 arr = np.zeros((5, 5), int)
 print(arr)
--- Day 1/Exercise5.py
+++ Day 1/Exercise5.py
@@ -1,25 +1,3 @@
-'''
-Exercise 5
-
-Using NumPy, create a 8x8 two-dimensional array - identity matrix with int16 data type and assign it to a variable named arr. In response, print this array to the console as shown below.
-
-
-Expected output:
-
-    [[1 0 0 0 0 0 0 0]
-     [0 1 0 0 0 0 0 0]
-     [0 0 1 0 0 0 0 0]
-     [0 0 0 1 0 0 0 0]
-     [0 0 0 0 1 0 0 0]
-     [0 0 0 0 0 1 0 0]
-     [0 0 0 0 0 0 1 0]
-     [0 0 0 0 0 0 0 1]]
-
-     
-Tip: Use the np.eye() function.
-
-'''
-
 import numpy as np
 
 arr = np.eye(8, dtype='int16')
